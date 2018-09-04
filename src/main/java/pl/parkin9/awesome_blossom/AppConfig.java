0baed9a9d383fc7bf57a6f8777e04e3fddbd34e3
Piotr <piotrparkola@gmail.com>
2018-09-04 15:28:16 +0200

    Add connection to MySQL

--- src/main/java/pl/parkin9/awesome_blossom/AppConfig.java
+++ src/main/java/pl/parkin9/awesome_blossom/AppConfig.java
@@ -3,17 +3,36 @@ package pl.parkin9.awesome_blossom;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.CacheControl;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.persistence.EntityManagerFactory;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
-@ComponentScan("pl.parkin9.awesome_blossom")
+@ComponentScan(basePackages = "pl.parkin9.awesome_blossom")
 @EnableWebMvc
+@EnableJpaRepositories(basePackages = "pl.parkin9.awesome_blossom.Repository")
+@EnableTransactionManagement
 public class AppConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LocalEntityManagerFactoryBean entityManagerFactoryBean() {
+        LocalEntityManagerFactoryBean entityManagerFactoryBean = new LocalEntityManagerFactoryBean();
+        entityManagerFactoryBean.setPersistenceUnitName("awesomeBlossomPersistenceUnit");
+        return entityManagerFactoryBean;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
 
     @Bean
     public ViewResolver viewResolver() {
---
+++ src/main/java/pl/parkin9/awesome_blossom/Model/User.java
@@ -0,0 +1,57 @@
+package pl.parkin9.awesome_blossom.Model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "*Pole obowiązkowe.")
+    private String userName;
+
+    @NotBlank(message = "*Pole obowiązkowe.")
+    @Size(min = 6, message = "*Minimum 6 znaków.")
+    private String password;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
